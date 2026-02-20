e7b4e39e2cd367f5c5038df534814bd8b508caf0
Yakshawan <ggolem@naver.com>
2026-02-20 17:05:34 +0900

    주역점 텍스트 일부 수정

--- data/content.ts
+++ data/content.ts
@@ -32,10 +32,10 @@ export const heroContent = {
 export const projects: Project[] = [
     {
         id: "iching",
-        title: "주역점",
-        enTitle: "Daily I Ching",
+        title: "易: 주역으로 보는 오늘의 운세",
+        enTitle: "易: Daily I Ching",
         lastUpdated: "2026-02-20",
-        concept: "브라우저에서 즐기는 데일리 주역점 운세",
+        concept: "브라우저에서 즐기는 데일리 주역점",
         description: "크롬 사이드 패널에서 8면체 주사위를 굴려 64괘 중 하나를 뽑아볼 수 있는 무료 주역점 익스텐션. 매일 빠르고 간편하게 그날의 운세를 확인할 수 있다. 차분한 에디토리얼 UI, 5개 국어 지원.",
         techStack: ["React 18", "TypeScript", "Vite 5", "Three.js", "Chrome Extension MV3"],
         link: "https://chromewebstore.google.com/detail/leanlpamofllkckfmjiannjefkbbjheb",
