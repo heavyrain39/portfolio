5b464b523b972ebed3e4ca30d2eee8f578f246c5
Yakshawan <ggolem@naver.com>
2026-03-21 14:44:22 +0900

    마침표 추가

--- data/content.ts
+++ data/content.ts
@@ -56,7 +56,7 @@ export const projects: Project[] = [
         title: "마스터마인드",
         enTitle: "MSTRMND",
         lastUpdated: "2026-02-23",
-        concept: "유튜브 음원을 위한 영리한 마스터링 도구.",
+        concept: "유튜브 음원을 위한 영리한 마스터링 도구",
         enConcept: "A clever audio mastering tool for Youtube creators.",
         description: "다수의 트랙을 처리하는 작업자를 위해 만들어진 웹 기반 배치 마스터링 솔루션. 최대 50개 트랙 일괄 처리. 유튜브/스트리밍 최적화 볼륨 세팅과 6개 프리셋, 커스텀 설정 가능. 프로페셔널한 사운드를 무료로, 빠르게 완성하자.",
         enDescription: "A web-based batch mastering solution built for creators who handle multiple tracks. Processes up to 50 tracks at once. Features default Youtube/Streaming volume optimization, 6 presets, and custom settings. Achieve professional sound for free, fast.",
