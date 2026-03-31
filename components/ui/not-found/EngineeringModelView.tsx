23f6d3bec74cd208368af8af1e6ceb5ee5ea3995
Yakshawan <ggolem@naver.com>
2026-04-01 02:39:53 +0900

    3d 모델 투명도 약화 2.5에서 1.5로

--- components/ui/not-found/EngineeringModelView.tsx
+++ components/ui/not-found/EngineeringModelView.tsx
@@ -32,7 +32,7 @@ function WireframeModel({
           color: wireColor,
           wireframe: true,
           transparent: true,
-          opacity: 0.25,
+          opacity: 0.15,
         });
       }
     });
